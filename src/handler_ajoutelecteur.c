e17ff2c955dbe0c652c03d0f83e01b3909490e2d
EvanGav <evan.yosseph@gmail.com>
2024-01-18 12:19:34 +0100

    handlervote

--- src/handler_ajoutelecteur.c
+++ src/handler_ajoutelecteur.c
@@ -1,23 +1,17 @@
-<<<<<<< HEAD:src/handler_ajoutelecteur.c
 
 #include "handler.h"
-=======
 #include <stdio.h>
 #include "../../common/include/messages.h"
 #include "../../common/include/bd.h"
->>>>>>> ec704c9eda8bf0f4a92eb2cc0a1f01cab3e63163:src/handler/handler_ajoutelecteur.c
 
 // Fonction pour ajouter un électeur
 int handlerajoutelecteur(sqlite3 *db, Commande *cmd) {
     char *id = cmd->commande.ajoutElecteur.identifiant;
     if (electeurExists(db, id, strlen(id) + 1) == 0)
     {
-<<<<<<< HEAD:src/handler_ajoutelecteur.c
         createElecteur(db, id, strlen(id)+1);
         printf("Electeur creer");
-=======
         createElecteur(db, id, strlen(id) + 1);
->>>>>>> ec704c9eda8bf0f4a92eb2cc0a1f01cab3e63163:src/handler/handler_ajoutelecteur.c
         return 0;
     }
     return -1;
