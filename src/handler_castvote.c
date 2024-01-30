799428e387acfaf95fb15a6bd780df784e90222e
EvanGav <evan.yosseph@gmail.com>
2024-01-30 01:34:23 +0100

    db cancels

--- src/handler_castvote.c
+++ src/handler_castvote.c
@@ -8,7 +8,7 @@
 int handlercastvote(sqlite3 *db, Commande *cmd) {
     char *id = cmd->commande.castVote.identifiant;
     char* idElection = cmd->commande.castVote.idElection;
-    if (electeurExists(db, id, strlen(id) + 1) == 0 && electionExists(db, idElection, strlen(idElection) + 1) == 0)
+    if (electeurExists(db, id, strlen(id) + 1) == 0 && Election_getIdFromNumeroID(db, idElection, strlen(idElection) + 1) == 0)
     {
         Election_castVote(db, id, idElection, cmd->commande.castVote.ballot, cmd->commande.castVote.ballotSize, 1);
         return 0;
